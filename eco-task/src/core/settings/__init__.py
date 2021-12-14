ac0b7cf611cb57e09d84ee0caa16bd3786899765
abhijit shete <abhijitshete13@gmail.com>
2021-12-14 13:58:11 +0530

    deploy celery task schedule

--- eco-task/src/core/settings/__init__.py
+++ eco-task/src/core/settings/__init__.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,3 +126,5 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME':timedelta(days=1),
 }
+
+django_heroku.settings(local())
