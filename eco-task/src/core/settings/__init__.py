5eaa463bc94cd566353f8ddab12170fa1926e5c2
abhijit shete <abhijitshete13@gmail.com>
2021-12-15 10:07:46 +0530

    error resolve

--- eco-task/src/core/settings/__init__.py
+++ eco-task/src/core/settings/__init__.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,5 +125,3 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME':timedelta(days=1),
 }
-
-django_heroku.settings(local())
