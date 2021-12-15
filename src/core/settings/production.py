d776f4a6babd8970f32d34ad446a415a9c8a9189
abhijit shete <abhijitshete13@gmail.com>
2021-12-15 10:45:45 +0530

    connection setup

--- src/core/settings/production.py
+++ src/core/settings/production.py
@@ -1,5 +1,6 @@
 import os
 import dj_database_url
+import psycopg2
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get(
@@ -12,6 +13,13 @@ CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL')
 
 #DEFAULT_CONNECTION = dj_database_url.parse(os.environ.get(os.environ.get("DATABASE_URL_CONFIG")))
 DEFAULT_CONNECTION = dj_database_url.parse(os.environ.get("DATABASE_URL"))
+
+conn=psycopg2.connect(
+  database="eco",
+  user="postgres",
+  host="/tmp/",
+  password="postgres"
+)
 
 DEFAULT_CONNECTION.update({"CONN_MAX_AGE": 600})
 DATABASES = {"default": DEFAULT_CONNECTION}
